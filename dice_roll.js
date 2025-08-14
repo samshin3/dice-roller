ea8f00074a8af89afec4227611f2f761e39ebb6a
Sam Seung Min Shin <shinseungmin9498@gmail.com>
2025-08-13 20:13:04 -0400

    initial commit

---
+++ dice_roll.js
@@ -0,0 +1,24 @@
+function roll_dice(num_sides) {
+    const min = 1;
+    const inclusive = num_sides + 1;
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(inclusive);
+    val = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+    return val;
+}
+
+function main() {
+    const num_sides = Number(document.getElementById("num_sides").value);
+    const num_dice = Number(document.getElementById("num_dice").value);
+    const threshold = Number(document.getElementById("threshold").value);
+    var count = 0;
+
+    for (let a = 0; a < num_dice; a++) {
+        let dice_val = roll_dice(num_sides);
+
+        if (dice_val >= threshold) {
+            count++;
+        }
+    }
+    alert("There are " + count + " dice that rolled " + threshold + " or greater");
+}
